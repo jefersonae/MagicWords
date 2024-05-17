93d8eadd18c850f855c5edda7efa8fbe76c2dd38
Jeferson de Souza Andrade <38109358+jefersonae@users.noreply.github.com>
2024-05-17 12:19:46 -0300

    Add files via upload

---
+++ src/main/java/com/magicwords/MagicwordsApplication.java
@@ -0,0 +1,13 @@
+package com.magicwords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicwordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicwordsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/magicwords/MagicwordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magicwords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicwordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
