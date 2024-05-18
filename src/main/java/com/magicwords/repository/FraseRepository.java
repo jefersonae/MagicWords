3099a50d5c8de8892da0808ec8cbc188b11c37a0
Jeferson de Souza Andrade <38109358+jefersonae@users.noreply.github.com>
2024-05-18 13:57:41 -0300

    Final project

--- src/main/java/com/magicwords/repository/FraseRepository.java
+++ src/main/java/com/magicwords/repository/FraseRepository.java
@@ -2,7 +2,9 @@ package com.magicwords.repository;
 
 import com.magicwords.model.Frase;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface FraseRepository extends JpaRepository<Frase, Long> {
-
+    @Query("SELECT f FROM Frase f order by function('RANDOM') LIMIT 1")
+    Frase buscaFraseAleatoria();
 }
--- src/main/java/com/magicwords/service/FraseService.java
+++ src/main/java/com/magicwords/service/FraseService.java
@@ -11,7 +11,7 @@ public class FraseService {
     @Autowired
     private FraseRepository repositorio;
     public FraseDTO  obterFraseAleatoria(){
-        Frase frase = repositorio.findById(4L).get();
+        Frase frase = repositorio.buscaFraseAleatoria();
         return new FraseDTO(frase.getTitulo(), frase.getFrase(), frase.getPersonagem(), frase.getPoster());
     }
 }
